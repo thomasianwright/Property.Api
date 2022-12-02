ebf2083f08d6b2d2c8917fe39396088a0f2651e4
Tom <Thomas.Wright@caliqon.com>
2022-12-02 13:11:58 +0000

    Added password reset migrations

--- src/Property.Api.Infrastructure/Migrations/20221202131134_AddedPasswordResetTable.Designer.cs
+++ src/Property.Api.Infrastructure/Migrations/20221202131134_AddedPasswordResetTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Property.Api.Infrastructure.Data;
 
@@ -11,9 +12,11 @@ using Property.Api.Infrastructure.Data;
 namespace Property.Api.Infrastructure.Migrations
 {
     [DbContext(typeof(ApiContext))]
-    partial class ApiContextModelSnapshot : ModelSnapshot
+    [Migration("20221202131134_AddedPasswordResetTable")]
+    partial class AddedPasswordResetTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
