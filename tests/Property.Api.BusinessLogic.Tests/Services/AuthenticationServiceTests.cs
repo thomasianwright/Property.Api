b61915d3e74be654af321cb0fe7fe0ab7d47dce9
Tom <Thomas.Wright@caliqon.com>
2022-12-02 10:52:22 +0000

    Updated tests

--- tests/Property.Api.BusinessLogic.Tests/Services/AuthenticationServiceTests.cs
+++ tests/Property.Api.BusinessLogic.Tests/Services/AuthenticationServiceTests.cs
@@ -40,7 +40,7 @@ public class AuthenticationServiceTests
             },
             Password = null
         };
-        
+
         var user = await authenticationService.Register(registerDto);
 
         emailService.Verify(x => x.SendNewUserEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()),
@@ -54,5 +54,50 @@ public class AuthenticationServiceTests
         user.Address.Postcode.Should().BeSameAs(registerDto.Address.Postcode);
         user.Address.Country.Should().BeSameAs(registerDto.Address.Country);
         user.Address.County.Should().BeSameAs(registerDto.Address.County);
+    }
+
+    [Fact]
+    public async Task Assert_Signup_Of_User_With_Password_Does_Not_Send_Password_Via_EmailService()
+    {
+        var emailService = new Mock<IEmailService>();
+        var userService = new Mock<IUserRepository>();
+        var mapper = new Mapper(new MapperConfiguration((config =>
+        {
+            config.AddProfiles(new Profile[]
+            {
+                new UserMapping(), new AddressMapping(), new AccountMapping(), new CompanyMapping(),
+                new PropertyMapping(), new AccountMapping(), new RentalAgreementMapping()
+            });
+        })));
+
+        var authenticationService = new AuthenticationService(userService.Object, mapper, emailService.Object);
+
+        emailService.Setup(x => x.SendNewUserEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(Task.CompletedTask);
+
+        var registerDto = new CreateUserDto
+        {
+            FirstName = "Thomas",
+            LastName = "Wright",
+            Email = "thomas.wright@example.com",
+            Address = new CreateAddressDto
+            {
+                LineOne = "123 Fake Street",
+                LineTwo = "Fake Town",
+                Postcode = "FAKE1",
+                Country = "Fake Country",
+                County = "Fake County"
+            },
+            Password = "TestPass"
+        };
+
+        var user = await authenticationService.Register(registerDto);
+
+        emailService.Verify(x => x.SendNewUserEmail(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()),
+            Times.Never);
+
+        user!.FirstName.Should().BeSameAs(registerDto.FirstName);
+        user.LastName.Should().BeSameAs(registerDto.LastName);
+        user.Email.Should().BeSameAs(registerDto.Email);
     }
 }
